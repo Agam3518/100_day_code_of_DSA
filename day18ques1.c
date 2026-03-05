515b5a6f8069a16827b919f15b944797ca395b8b
Agam3518 <agam.k3518@gmail.com>
2026-03-06 02:14:48 +0530

    Add files via upload

---
+++ day18ques1.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+void reverse(int arr[], int start, int end, int n) {
+    printf("Reversing from index %d to %d\n", start, end);
+
+    while(start < end) {
+        printf("Swapping arr[%d]=%d and arr[%d]=%d\n",
+               start, arr[start], end, arr[end]);
+
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    printf("Array after this reverse step: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter array elements:\n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        printf("arr[%d] = %d\n", i, arr[i]);
+    }
+
+    int k;
+    printf("Enter value of k: ");
+    scanf("%d", &k);
+
+    printf("Original k = %d\n", k);
+
+    k = k % n;
+    printf("Effective k after mod = %d\n", k);
+
+    printf("\nStep 1: Reverse entire array\n");
+    reverse(arr, 0, n - 1, n);
+
+    printf("\nStep 2: Reverse first k elements\n");
+    reverse(arr, 0, k - 1, n);
+
+    printf("\nStep 3: Reverse remaining elements\n");
+    reverse(arr, k, n - 1, n);
+
+    printf("\nFinal Rotated Array:\n");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
